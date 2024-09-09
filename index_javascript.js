c399968fc740e7aab26bb3560dffd000a2c23cd4
kevin73633 <kevindeepak@hotmail.com>
2024-09-09 23:27:04 +0800

    removed some unnecessary files and added ability to push data to firebase rt database

--- index_javascript.js
+++ index_javascript.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import  { getDatabase, ref, set }  from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,62 +17,72 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
 console.log(app);
-$(document).ready(function(){
-	// function myFunction() {
+function writeUserData(data) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + "bruh"), {
+    data: data
+  });
+}
+writeUserData("Test");
+// $(document).ready(function(){
+// 	// function myFunction() {
 		
-	// 	if (($('h1').is(':hover'))){
+// 	// 	if (($('h1').is(':hover'))){
 			
-	// 		$('h1').css('transition','all .2s ease-in-out');
-	// 		$('h1').css('transform','scale(1.05,1.05)');
-	// 	} 
-	// 	else {
-	// 		$('h1').css('transform','scale(1.0,1.0)');
-	// 	}
-	// 	if (($("#div1").is(':hover')) ){
-	// 		$("#div1").css('transition','all .1s ease-in-out');
-	// 		$("#div1").css('transform','scale(1.02,1.02)');
-	// 	} 
-	// 	else {
-	// 		$("#div1").css('transform','scale(1.0,1.0)');
-	// 	}
-	// 	if (($("#div3").is(':hover')) ){
-	// 		$("#div3").css('transition','all .1s ease-in-out');
-	// 		$("#div3").css('transform','scale(1.02,1.02)');
-	// 		$("#gif1").css('margin-right','0%');
+// 	// 		$('h1').css('transition','all .2s ease-in-out');
+// 	// 		$('h1').css('transform','scale(1.05,1.05)');
+// 	// 	} 
+// 	// 	else {
+// 	// 		$('h1').css('transform','scale(1.0,1.0)');
+// 	// 	}
+// 	// 	if (($("#div1").is(':hover')) ){
+// 	// 		$("#div1").css('transition','all .1s ease-in-out');
+// 	// 		$("#div1").css('transform','scale(1.02,1.02)');
+// 	// 	} 
+// 	// 	else {
+// 	// 		$("#div1").css('transform','scale(1.0,1.0)');
+// 	// 	}
+// 	// 	if (($("#div3").is(':hover')) ){
+// 	// 		$("#div3").css('transition','all .1s ease-in-out');
+// 	// 		$("#div3").css('transform','scale(1.02,1.02)');
+// 	// 		$("#gif1").css('margin-right','0%');
 		
-	// 	} 
-	// 	else {
-	// 		$("#div3").css('transform','scale(1.0,1.0)');
-	// 		$("#gif1").css('margin-right','-50%');
-	// 		$("#div3").css("overflow", "hidden");
-	// 	}
-	// 	if (($("#div2").is(':hover')) ){
-	// 		$("#div2").css('transition','all .1s ease-in-out');
-	// 		$("#div2").css('transform','scale(1.02,1.02)');
-	// 		$("#gif2").css('margin-left','0%');
+// 	// 	} 
+// 	// 	else {
+// 	// 		$("#div3").css('transform','scale(1.0,1.0)');
+// 	// 		$("#gif1").css('margin-right','-50%');
+// 	// 		$("#div3").css("overflow", "hidden");
+// 	// 	}
+// 	// 	if (($("#div2").is(':hover')) ){
+// 	// 		$("#div2").css('transition','all .1s ease-in-out');
+// 	// 		$("#div2").css('transform','scale(1.02,1.02)');
+// 	// 		$("#gif2").css('margin-left','0%');
 			
-	// 	} 
-	// 	else {
-	// 		$("#div2").css('transform','scale(1.0,1.0)');
-	// 		$("#gif2").css('margin-left','-50%');
-	// 		$("#div2").css("overflow", "hidden");
-	// 	}
-	// 	if (($("#div4").is(':hover'))){
-	// 		$("#div4").css('transition','all .1s ease-in-out');
-	// 		$("#div4").css('transform','scale(1.02,1.04)');
-	// 		$("#gif3").css('margin-right','0%');
+// 	// 	} 
+// 	// 	else {
+// 	// 		$("#div2").css('transform','scale(1.0,1.0)');
+// 	// 		$("#gif2").css('margin-left','-50%');
+// 	// 		$("#div2").css("overflow", "hidden");
+// 	// 	}
+// 	// 	if (($("#div4").is(':hover'))){
+// 	// 		$("#div4").css('transition','all .1s ease-in-out');
+// 	// 		$("#div4").css('transform','scale(1.02,1.04)');
+// 	// 		$("#gif3").css('margin-right','0%');
 			
-	// 	} 
-	// 	else {
-	// 		$("#div4").css('transform','scale(1.0,1.0)');
-	// 		$("#gif3").css('margin-right','-50%');
-	// 		$("#div4").css("overflow", "hidden");
-	// 	}
-	// }
+// 	// 	} 
+// 	// 	else {
+// 	// 		$("#div4").css('transform','scale(1.0,1.0)');
+// 	// 		$("#gif3").css('margin-right','-50%');
+// 	// 		$("#div4").css("overflow", "hidden");
+// 	// 	}
+// 	// }
 	
 
-	// window.onmousemove  = function() {myFunction()};
+// 	// window.onmousemove  = function() {myFunction()};
 	
 	
-});
+// });
