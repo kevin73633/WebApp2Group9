f0af136dc4fe82871a1cb385d7e9980a7df6348a
kevin73633 <kevindeepak@hotmail.com>
2024-09-10 20:24:53 +0800

    changed login to using google

--- index_javascript.js
+++ index_javascript.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import  { getDatabase, ref, get, set, child, onValue, onChildAdded, onChildChanged, onChildRemoved}  from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
-import  { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut}  from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import  { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut}  from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -54,24 +54,48 @@ onChildRemoved(usersRef, (snapshot) => {
 //AUTH
 const auth = getAuth();
 const form = document.getElementById('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
- signInWithEmailAndPassword(auth, email, password)
-  .then((user) => {
-      if (user) {
-        alert('sucess');
-      }
-      // ...
-  })
-  .catch((error) => {
-      console.log("error: email or password is incorrect");
+const provider = new GoogleAuthProvider();
+document.getElementById('GoogleLoginBtn').addEventListener('click',function(){
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
   });
-  
 });
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const email = document.getElementById('email').value;
+//   const password = document.getElementById('password').value;
+
+//  signInWithEmailAndPassword(auth, email, password)
+//   .then((user) => {
+//       if (user) {
+//         alert('sucess');
+//       }
+//       // ...
+//   })
+//   .catch((error) => {
+//       console.log("error: email or password is incorrect");
+//   });
+  
+// });
+
 function logout () {
   signOut(auth).then(() => {
       // Sign-out successful.
