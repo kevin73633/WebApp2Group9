2fcd2c27f44d5c4e53fd5d3c78026a86e46d1619
ongkenith <kenith.ong.2023@scis.smu.edu.sg>
2024-10-12 15:47:26 +0800

    121024 - Creation of filter options
    1. Created function to remove the rows in the table after "Search" button has been pressed for the first time
    2. Created function to automate filtering of options
    3. Added a way to increase or decrease categories filtering based on searched results

--- catalog.js
+++ catalog.js
@@ -1,26 +1,53 @@
+// Example Data
+const exampleData = [
+    {"name": "Introduction to Programming", "codeNo": "111", "category": "IS Core", "status": "yes", "enrolled_year": "Y1"},
+    {"name": "Object Oriented Programming", "codeNo": "102", "category": "CS Core", "status": "yes", "enrolled_year": "Y2"},
+    {"name": "Data Mangement", "codeNo": "112", "category": "IS Core", "status": "no", "enrolled_year": null}
+]
+
+// All the options in the filter
+const Categories = ["All Tracks"]
+
+/* This document contains functions: createModulesTable(), removeTableItems(tableModules) */
+
+function createFilterOptions() {
+    // <option selected>All Tracks</option>
+    //   <option value="1">...</option>
+    //   <option value="2">..</option>
+
+    var select = document.getElementById("filterOptions");
+
+    for (let category of Categories) {
+        var options = document.createElement("option");
+        options.setAttribute("value", category);
+        options.innerText = category;
+        select.appendChild(options);
+    }
+}
+
 function createModulesTable() {
-    /* This function creates the table of modules when user pressed "Search" button*/
+    /*
+        This function creates the table of modules when user pressed "Search" button
+        Also changes the filter dropdown options
+    */
 
     // Table example
-        // <td><input type="checkbox"></td>
-        // <td>Introduction to Programming</td>
-        // <td>111</td>
-        // <td>IS Core</td>
-        // <td>Enrolled Y1</td>
-        // <td><button type="button" class="btn btn-secondary px-3 rounded-2">View More</button></td>
-
-    // Example Data
-    const exampleData = [
-        {"name": "Introduction to Programming", "codeNo": "111", "category": "IS Core", "status": "yes", "enrolled_year": "Y1"},
-        {"name": "Object Oriented Programming", "codeNo": "102", "category": "CS Core", "status": "yes", "enrolled_year": "Y2"},
-        {"name": "Data Mangement", "codeNo": "112", "category": "IS Core", "status": "no", "enrolled_year": null}
-    ]
+    //   <td><input type="checkbox"></td>
+    //   <td>Introduction to Programming</td>
+    //   <td>111</td>
+    //   <td>IS Core</td>
+    //   <td>Enrolled Y1</td>
+    //   <td><button type="button" class="btn btn-secondary px-3 rounded-2">View More</button></td>
 
     // Get Table Element
     var tableModules = document.getElementById("tableModules");
 
+    // Resets table
+    removeTableItems(tableModules)
+
     // Create Element: Table Body
     var tableBody = document.createElement("tbody")
+    tableBody.setAttribute("id", "listModules")
 
     for (let data_item of exampleData) {
         //Create Table Row
@@ -56,7 +83,7 @@ function createModulesTable() {
         var col = document.createElement("td");
         // For enrolled or not, use if-else
         if (tookCourse === "yes") {
-            col.innerText = "Enrolled" + enrolledYear;
+            col.innerText = "Enrolled " + enrolledYear;
             col.setAttribute("class", "text-success");
         }
         else {
@@ -74,10 +101,31 @@ function createModulesTable() {
         tableDataCol.appendChild(button);
         row.appendChild(tableDataCol);
 
-        // Append column to table
+        // Append column to row
         tableBody.appendChild(row);
+
+        // Add category option if not in list yet
+        if (Categories.indexOf(category) === -1) {
+            Categories.push(category);
+        }
     }
 
-    tableModules.appendChild(tableBody)
-    
+    //Append row to table
+    tableModules.appendChild(tableBody);
+
+    // Create new filter
+    createFilterOptions();
 }
+
+function removeTableItems(tableModules) {
+    /* This functions removes the items in the table if the search button has been pressed before*/
+
+    // Get table rows
+    var rows = document.getElementById("listModules");
+
+    if (rows !== null) {
+        tableModules.children[1].remove();
+    }
+}
+
+createFilterOptions()
