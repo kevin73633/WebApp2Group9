241d3c203b6de2020c07a3d71ccdc34155d9726c
Hnin Thanda Kyaw <tk.hnin.2023@scis.smu.edu.sg>
2024-11-07 12:36:55 +0800

    planner borders

--- planner.js
+++ planner.js
@@ -77,7 +77,8 @@ function ShowPlanner2()
             var currHeader = headers.getElementsByTagName("th");
             if (currHeader[index].getAttribute("value") == global.currUser.GetAllCourseYearAndSemTaken()[course])
             {
-                td.style.backgroundColor = "red";
+                td.style.backgroundColor = "#0c1547";
+                td.style.borderRadius = "10px";
             }
             row.appendChild(td);
         }
