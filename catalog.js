828e56af46533f94bd8034b3d0cea8f84cdc6f82
TIMOTHY LOO <timothy.loo.2023@scis.smu.edu.sg>
2024-11-12 10:35:16 +0800

    css making it neater

--- catalog.js
+++ catalog.js
@@ -238,7 +238,7 @@ function createCourseModal(course)
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="Modal_${course.courseCode}">${course.courseName}</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     ${course.courseDescription}
